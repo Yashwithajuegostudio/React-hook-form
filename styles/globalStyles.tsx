cd2bd67244711b176677f70e0d9d6686fbbf6dbc
yashwitha-at-411977029542 <D/b20m+IOckszuzuPuS4+r+Yorqu2R1a8ymbIA6yjhI=>
2023-03-15 10:39:06 +0530

    FEAT:form Ui styling

---
+++ styles/globalStyles.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const FormInputContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  margin-bottom: 0.7rem;
+  gap: 1rem;
+`;
+
+export const FormFooterContainer = styled.main`
+  gap: 1rem;
+
+  display: flex;
+`;
