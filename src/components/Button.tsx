92a01460ce1580b6eef5e5b563c939b4a1a19627
yashwitha-at-411977029542 <D/b20m+IOckszuzuPuS4+r+Yorqu2R1a8ymbIA6yjhI=>
2023-03-15 10:38:37 +0530

    FEAT:Button Component UI styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,32 @@
 import { FC } from "react";
+import styled from "styled-components";
 
-const Button: FC<any> = ({ type, name, text }) => {
+interface ButtonProps {
+  type?: "button" | "submit" | "reset" | undefined;
+  onChange?: React.FormEventHandler<HTMLButtonElement> | undefined;
+  value?: string | number | readonly string[] | undefined;
+  onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined;
+  variant?: "primary" | "secondary";
+  width?: "full" | "custom";
+  borderType?: "halfCurved";
+  disabled?: boolean;
+  style?: object;
+  backgroundColor?: string;
+  color?: string;
+  border?: string;
+  name?: string;
+  text?: string;
+}
+const Button: FC<ButtonProps> = ({ type, name, text, width }) => {
   return (
     // <button {...(register && register(name, rules))} type={type} name={name}>
-    <button type={type} name={name}>
+    <ButtonStyle type={type} name={name} width={width}>
       {text}
-    </button>
+    </ButtonStyle>
   );
 };
 export default Button;
+const ButtonStyle = styled.button<ButtonProps>`
+  width: ${(props) => (props.width === "full" ? "100%" : `auto`)};
+  padding: 0.5rem 1.5rem;
+`;
