a9950e3434f3ebc3f4ac1ad551597c283da46310
yashwitha-at-411977029542 <D/b20m+IOckszuzuPuS4+r+Yorqu2R1a8ymbIA6yjhI=>
2023-03-15 10:34:15 +0530

    FEAT:Form UI styling

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,30 +1,43 @@
 import React, { FC } from "react";
 import { useForm } from "react-hook-form";
+import styled from "styled-components";
 interface FormProps {
   children: JSX.Element[];
   onSubmit: Function | any;
   onError: Function | any;
-  style: object;
 }
-const Form: FC<FormProps> = ({ children, onSubmit, onError, style }) => {
+
+const Form: FC<FormProps> = ({ children, onSubmit, onError }) => {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
   return (
-    <form onSubmit={handleSubmit(onSubmit, onError)} style={style}>
+    <FormContainer onSubmit={handleSubmit(onSubmit, onError)}>
       {children?.map((element: JSX.Element, index: number) =>
-        element?.props?.children?.length > 1 ? (
-          <div key={index} style={{ display: "flex", flexDirection: "row" }}>
-            {typeof element?.props?.children == "object" &&
-              element?.props?.children.map((subChild: JSX.Element) => {
-                return {
-                  ...subChild,
-                  props: { ...subChild.props, register, errors },
-                };
-              })}
-          </div>
+        Object.keys(element?.props?.children || {}).length > 0 ? (
+          element?.type?.target === "div" ? (
+            <FormBodyContainer key={index}>
+              {typeof element?.props?.children == "object" &&
+                element?.props?.children.map((subChild: JSX.Element) => {
+                  return {
+                    ...subChild,
+                    props: { ...subChild.props, register, errors },
+                  };
+                })}
+            </FormBodyContainer>
+          ) : (
+            <FormFooterContainer>
+              {typeof element?.props?.children == "object" &&
+                element?.props?.children.map((subChild: JSX.Element) => {
+                  return {
+                    ...subChild,
+                    props: { ...subChild.props, register, errors },
+                  };
+                })}
+            </FormFooterContainer>
+          )
         ) : (
           {
             ...element,
@@ -32,7 +45,33 @@ const Form: FC<FormProps> = ({ children, onSubmit, onError, style }) => {
           }
         )
       )}
-    </form>
+    </FormContainer>
   );
 };
 export default Form;
+const FormContainer = styled.form`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  > * {
+    flex: 0 0 50%;
+  }
+`;
+
+const FormBodyContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const FormFooterContainer = styled.main`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+`;
